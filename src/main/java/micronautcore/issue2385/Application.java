a82b2b68286fe15bf5c9f43f71455b486373ef8d
Puneet Behl <behl4485@gmail.com>
2019-12-16 15:59:12 +0530

    Initial Micronaut Application

---
+++ src/main/java/micronautcore/issue2385/Application.java
@@ -0,0 +1,10 @@
+package micronautcore.issue2385;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
